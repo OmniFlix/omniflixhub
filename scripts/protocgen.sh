71ae3e6d01eb6d6318e8f9686a35a03e337509de
Harish Marri <harishmarri551@gmail.com>
2023-03-08 15:21:13 +0530

    update protoc gen script

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -3,13 +3,14 @@
 set -eo pipefail
 
 # get protoc executions
-go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
+#go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 # get cosmos sdk from github
-go get github.com/cosmos/cosmos-sdk 2>/dev/null
+#go get github.com/cosmos/cosmos-sdk 2>/dev/null
 
 # Get the path of the cosmos-sdk repo from go/pkg/mod
 cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
-proto_dirs=$(find . -path ./third_party -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+echo $cosmos_sdk_dir;
+proto_dirs=$(find ./proto -path ./third_party -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate protobuf bind
   protoc \
