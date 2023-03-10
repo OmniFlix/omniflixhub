a7f2f6dd00af4a96842d3a2c6e0d5437ecd5c366
Harish Marri <harishmarri551@gmail.com>
2023-03-11 02:06:16 +0530

    itc: add genesis validation

---
+++ x/itc/types/genesis.go
@@ -0,0 +1,37 @@
+package types
+
+import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+func NewGenesisState(campaigns []Campaign,
+	params Params, claims []Claim,
+	nextCampaignNumber uint64) *GenesisState {
+	return &GenesisState{
+		Campaigns:          campaigns,
+		Claims:             claims,
+		Params:             params,
+		NextCampaignNumber: nextCampaignNumber,
+	}
+}
+
+func (m *GenesisState) ValidateGenesis() error {
+	for _, c := range m.Campaigns {
+		if c.GetCreator().Empty() {
+			return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "missing campaign creator address")
+		}
+		if err := ValidateCampaign(c); err != nil {
+			return err
+		}
+	}
+	for _, claim := range m.Claims {
+		if err := ValidateClaim(claim); err != nil {
+			return err
+		}
+	}
+	if err := m.Params.ValidateBasic(); err != nil {
+		return err
+	}
+	if m.NextCampaignNumber <= 0 {
+		return sdkerrors.Wrap(ErrNonPositiveNumber, "must be a number and greater than 0.")
+	}
+	return nil
+}
