91f4c76d9a9152785c0efb0636b8a01ba6f8cacc
Harish Marri <harishmarri551@gmail.com>
2023-06-12 21:40:46 +0530

    add v0.11.0 upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -3,10 +3,6 @@ package app
 import (
 	"fmt"
 
-	alloctypes "github.com/OmniFlix/omniflixhub/x/alloc/types"
-
-	onfttypes "github.com/OmniFlix/onft/types"
-
 	store "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -14,63 +10,16 @@ import (
 )
 
 // next upgrade name
-const upgradeName = "v0.10.0"
-
-const (
-	developerRewardsAccount    = "omniflix1ftvf4euvdvq95jpeyvgf6r6j78j5rct2a3jnkn"
-	nftIncentivesAccount       = "omniflix139qa9tklr4trzvugqm5ycvky80px90yn5hs3kc"
-	nodeHostsIncentivesAccount = "omniflix1djc90zwkk2vaqryne8c68f2tkp6u9ug9qfrnh8"
-)
+const upgradeName = "v0.11.0"
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
-	app.UpgradeKeeper.SetUpgradeHandler(upgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
-		developerRewardsAcc, err := sdk.AccAddressFromBech32(developerRewardsAccount)
-		if err != nil {
-			panic(err)
-		}
-		nftIncentivesAcc, err := sdk.AccAddressFromBech32(nftIncentivesAccount)
-		if err != nil {
-			panic(err)
-		}
-		nodeHostsIncentivesAcc, err := sdk.AccAddressFromBech32(nodeHostsIncentivesAccount)
-		if err != nil {
-			panic(err)
-		}
-
-		app.ONFTKeeper.SetParams(ctx, onfttypes.DefaultParams())
-		app.AllocKeeper.SetParams(ctx, alloctypes.Params{
-			DistributionProportions: alloctypes.DistributionProportions{
-				StakingRewards:      sdk.NewDecWithPrec(60, 2), // 60%
-				NftIncentives:       sdk.NewDecWithPrec(15, 2), // 15%
-				NodeHostsIncentives: sdk.NewDecWithPrec(5, 2),  // 5%
-				DeveloperRewards:    sdk.NewDecWithPrec(15, 2), // 15%
-				CommunityPool:       sdk.NewDecWithPrec(5, 2),  // 5%
-			},
-			WeightedDeveloperRewardsReceivers: []alloctypes.WeightedAddress{
-				{
-					Address: developerRewardsAcc.String(),
-					Weight:  sdk.NewDecWithPrec(100, 2),
-				},
-			},
-			WeightedNftIncentivesReceivers: []alloctypes.WeightedAddress{
-				{
-					Address: nftIncentivesAcc.String(),
-					Weight:  sdk.NewDecWithPrec(100, 2),
-				},
-			},
-			WeightedNodeHostsIncentivesReceivers: []alloctypes.WeightedAddress{
-				{
-					Address: nodeHostsIncentivesAcc.String(),
-					Weight:  sdk.NewDecWithPrec(100, 2),
-				},
-			},
+	app.UpgradeKeeper.SetUpgradeHandler(
+		upgradeName,
+		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			ctx.Logger().Info("running migrations ...")
+			return app.mm.RunMigrations(ctx, cfg, fromVM)
 		})
-
-		fromVM := app.mm.GetVersionMap()
-		ctx.Logger().Info("running migrations ...")
-		return app.mm.RunMigrations(ctx, cfg, fromVM)
-	})
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
