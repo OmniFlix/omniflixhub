e648cdcd926a820e129451b9c84c2cf837b3e9f9
Marri Harish <harishmarri551@gmail.com>
2023-08-04 17:26:41 +0530

    add upgrade handler for v0.12.x (#83)

--- app/upgrades/v012/constants.go
+++ app/upgrades/v012/constants.go
@@ -1,17 +1,18 @@
-package v2
+package v012
 
 import (
 	"github.com/OmniFlix/omniflixhub/app/upgrades"
 	store "github.com/cosmos/cosmos-sdk/store/types"
 	icahosttypes "github.com/cosmos/ibc-go/v4/modules/apps/27-interchain-accounts/host/types"
+	packetforwardtypes "github.com/strangelove-ventures/packet-forward-middleware/v4/router/types"
 )
 
-const UpgradeName = "v2"
+const UpgradeName = "v0.12.x"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{icahosttypes.StoreKey},
+		Added: []string{icahosttypes.StoreKey, packetforwardtypes.StoreKey},
 	},
 }
