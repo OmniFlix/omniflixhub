a136f58b4cb4c93bb7ba4ccfc20e42cf1156341c
Harish Marri <harishmarri551@gmail.com>
2023-09-12 15:34:14 +0530

    removed unused handler

--- x/alloc/migrations/v3/migrate.go
+++ x/alloc/migrations/v3/migrate.go
@@ -11,9 +11,7 @@ const (
 	ModuleName = "alloc"
 )
 
-var (
-	ParamsKey = []byte{0x01}
-)
+var ParamsKey = []byte{0x01}
 
 // Migrate migrates the x/alloc module state from the consensus version 1 to
 // version 2. Specifically, it takes the parameters that are currently stored
