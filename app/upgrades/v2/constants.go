67c1a2a7a698110a283c397c127bf1bd529e91f1
Harish Marri <harishmarri551@gmail.com>
2023-09-24 00:40:46 +0530

    add consensus and crisis modules to store upgrades

--- app/upgrades/v2/constants.go
+++ app/upgrades/v2/constants.go
@@ -3,6 +3,8 @@ package v2
 import (
 	"github.com/OmniFlix/omniflixhub/v2/app/upgrades"
 	store "github.com/cosmos/cosmos-sdk/store/types"
+	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 )
 
 const UpgradeName = "v2"
@@ -11,6 +13,9 @@ var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateV2UpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{},
+		Added: []string{
+			consensustypes.ModuleName,
+			crisistypes.ModuleName,
+		},
 	},
 }
