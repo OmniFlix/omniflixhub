bc296abdf0132d2f9ee0587c9d8051bb563ab88a
Marri Harish <harishmarri551@gmail.com>
2023-10-16 23:08:29 +0530

    add new modules in store upgrades list (#105)

--- app/upgrades/v2/constants.go
+++ app/upgrades/v2/constants.go
@@ -2,9 +2,13 @@ package v2
 
 import (
 	"github.com/OmniFlix/omniflixhub/v2/app/upgrades"
+	globalfeetypes "github.com/OmniFlix/omniflixhub/v2/x/globalfee/types"
+	tokenfactorytypes "github.com/OmniFlix/omniflixhub/v2/x/tokenfactory/types"
 	store "github.com/cosmos/cosmos-sdk/store/types"
 	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
 	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
+	"github.com/cosmos/cosmos-sdk/x/group"
+	icqtypes "github.com/cosmos/ibc-apps/modules/async-icq/v7/types"
 )
 
 const UpgradeName = "v2"
@@ -16,6 +20,10 @@ var Upgrade = upgrades.Upgrade{
 		Added: []string{
 			consensustypes.ModuleName,
 			crisistypes.ModuleName,
+			globalfeetypes.ModuleName,
+			group.ModuleName,
+			icqtypes.ModuleName,
+			tokenfactorytypes.ModuleName,
 		},
 	},
 }
