6ceb2454efbdb1a1879894ededa8500ee38cf8ee
Harish Marri <harishmarri551@gmail.com>
2023-11-20 21:39:09 +0530

    update upgrade handler for devnet-alpha

--- app/upgrades/v2/constants.go
+++ app/upgrades/v2/constants.go
@@ -2,29 +2,17 @@ package v2
 
 import (
 	"github.com/OmniFlix/omniflixhub/v2/app/upgrades"
-	globalfeetypes "github.com/OmniFlix/omniflixhub/v2/x/globalfee/types"
-	tokenfactorytypes "github.com/OmniFlix/omniflixhub/v2/x/tokenfactory/types"
 	ibcnfttransfertypes "github.com/bianjieai/nft-transfer/types"
 	store "github.com/cosmos/cosmos-sdk/store/types"
-	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
-	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
-	"github.com/cosmos/cosmos-sdk/x/group"
-	icqtypes "github.com/cosmos/ibc-apps/modules/async-icq/v7/types"
 )
 
-const UpgradeName = "v2"
+const UpgradeName = "v2-dev2"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateV2UpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
 		Added: []string{
-			consensustypes.ModuleName,
-			crisistypes.ModuleName,
-			globalfeetypes.ModuleName,
-			group.ModuleName,
-			icqtypes.ModuleName,
-			tokenfactorytypes.ModuleName,
 			ibcnfttransfertypes.ModuleName,
 		},
 	},
