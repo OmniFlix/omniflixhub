f13b12fcb6bea83cf8c81ff6a1ccdd8d723f6daa
Harish Marri <harishmarri551@gmail.com>
2023-11-24 02:25:10 +0530

    update onft migration

--- x/onft/migrations/v2/migrate.go
+++ x/onft/migrations/v2/migrate.go
@@ -14,10 +14,6 @@ const (
 
 var ParamsKey = []byte{0x07}
 
-// Migrate migrates the onft module state from the consensus version 1 to
-// version 2. Specifically, it takes the parameters that are currently stored
-// and managed by the x/params modules and stores them directly into the onft
-// module state.
 func Migrate(
 	ctx sdk.Context,
 	storeKey storetypes.StoreKey,
@@ -25,22 +21,37 @@ func Migrate(
 	cdc codec.BinaryCodec,
 	nftKeeper NFTKeeper,
 ) error {
-	var currParams types.Params
-	legacySubspace.GetParamSet(ctx, &currParams)
-
 	k := keeper{
 		storeKey:  storeKey,
 		cdc:       cdc,
 		nftKeeper: nftKeeper,
 	}
 
+	if err := MigrateParams(ctx, legacySubspace, k); err != nil {
+		return err
+	}
+	return MigrateCollections(ctx, storeKey, ctx.Logger(), k)
+}
+
+// MigrateParams migrates the onft module state from the consensus version 1 to
+// version 2. Specifically, it takes the parameters that are currently stored
+// and managed by the x/params modules and stores them directly into the onft
+// module state.
+func MigrateParams(
+	ctx sdk.Context,
+	legacySubspace exported.Subspace,
+	k keeper,
+) error {
+	var currParams types.Params
+	legacySubspace.GetParamSet(ctx, &currParams)
+
 	if err := currParams.ValidateBasic(); err != nil {
 		return err
 	}
 	store := ctx.KVStore(k.storeKey)
 
-	bz := cdc.MustMarshal(&currParams)
+	bz := k.cdc.MustMarshal(&currParams)
 	store.Set(ParamsKey, bz)
 
-	return MigrateCollections(ctx, storeKey, cdc, ctx.Logger(), k)
+	return nil
 }
