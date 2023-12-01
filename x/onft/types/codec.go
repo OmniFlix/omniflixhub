5c80865abdfb4e6981d8147a0340f9d6f3649a65
Harish Marri <harishmarri551@gmail.com>
2023-12-01 18:39:36 +0530

    update codec to support amino messages

--- x/onft/types/codec.go
+++ x/onft/types/codec.go
@@ -3,7 +3,9 @@ package types
 import (
 	"github.com/OmniFlix/omniflixhub/v2/x/onft/exported"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	authzcodec "github.com/cosmos/cosmos-sdk/x/authz/codec"
@@ -11,13 +13,14 @@ import (
 )
 
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgCreateDenom{}, "OmniFlix/onft/MsgCreateDenom", nil)
-	cdc.RegisterConcrete(&MsgUpdateDenom{}, "OmniFlix/onft/MsgUpdateDenom", nil)
-	cdc.RegisterConcrete(&MsgTransferDenom{}, "OmniFlix/onft/MsgTransferDenom", nil)
-	cdc.RegisterConcrete(&MsgTransferONFT{}, "OmniFlix/onft/MsgTransferONFT", nil)
-	cdc.RegisterConcrete(&MsgMintONFT{}, "OmniFlix/onft/MsgMintONFT", nil)
-	cdc.RegisterConcrete(&MsgBurnONFT{}, "OmniFlix/onft/MsgBurnONFT", nil)
-	cdc.RegisterConcrete(&MsgUpdateParams{}, "OmniFlix/onft/MsgUpdateParams", nil)
+
+	legacy.RegisterAminoMsg(cdc, &MsgCreateDenom{}, "OmniFlix/onft/MsgCreateDenom")
+	legacy.RegisterAminoMsg(cdc, &MsgUpdateDenom{}, "OmniFlix/onft/MsgUpdateDenom")
+	legacy.RegisterAminoMsg(cdc, &MsgTransferDenom{}, "OmniFlix/onft/MsgTransferDenom")
+	legacy.RegisterAminoMsg(cdc, &MsgTransferONFT{}, "OmniFlix/onft/MsgTransferONFT")
+	legacy.RegisterAminoMsg(cdc, &MsgMintONFT{}, "OmniFlix/onft/MsgMintONFT")
+	legacy.RegisterAminoMsg(cdc, &MsgBurnONFT{}, "OmniFlix/onft/MsgBurnONFT")
+	legacy.RegisterAminoMsg(cdc, &MsgUpdateParams{}, "OmniFlix/onft/MsgUpdateParams")
 
 	cdc.RegisterConcrete(&Params{}, "OmniFlix/onft/Params", nil)
 
@@ -51,15 +54,14 @@ func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 
 var (
 	amino     = codec.NewLegacyAmino()
-	ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+	ModuleCdc = codec.NewAminoCodec(amino)
 )
 
 func init() {
 	RegisterLegacyAminoCodec(amino)
-
+	cryptocodec.RegisterCrypto(amino)
 	sdk.RegisterLegacyAminoCodec(amino)
 	// Register all Amino interfaces and concrete types on the authz Amino codec so that this can later be
 	// used to properly serialize MsgGrant and MsgExec instances
 	RegisterLegacyAminoCodec(authzcodec.Amino)
-	amino.Seal()
 }
