593ff2c38e16163df8b3d78d3a1b704a689a8468
Harish Marri <harishmarri551@gmail.com>
2024-01-16 18:22:25 +0530

    add v2-upgrade-patch-1

---
+++ app/upgrades/v2-testnet-patch-1/constants.go
@@ -0,0 +1,16 @@
+package v2_testnet_patch_1
+
+import (
+	"github.com/OmniFlix/omniflixhub/v2/app/upgrades"
+	store "github.com/cosmos/cosmos-sdk/store/types"
+)
+
+const UpgradeName = "v2-testnet-patch-1"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateV2UpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{},
+	},
+}
