cfecb89817db43dd3ae3cf6d1da3a4438f9586a0
Harish Marri <harishmarri551@gmail.com>
2024-02-12 18:29:12 +0530

    update v3 upgrade handler for testnet

--- app/upgrades/v3/constants.go
+++ app/upgrades/v3/constants.go
@@ -6,7 +6,7 @@ import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
 )
 
-const UpgradeName = "v3-dev"
+const UpgradeName = "v3"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
