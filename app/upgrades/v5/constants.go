a5462bab6886193ae97da5547e38b729aa4db357
Harish Marri <harishmarri551@gmail.com>
2024-08-02 22:51:12 +0530

    add v5 upgrade handler

---
+++ app/upgrades/v5/constants.go
@@ -0,0 +1,16 @@
+package v4
+
+import (
+	store "cosmossdk.io/store/types"
+	"github.com/OmniFlix/omniflixhub/v5/app/upgrades"
+)
+
+const UpgradeName = "v5"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateV5UpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{},
+	},
+}
---
+++ app/upgrades/v5/upgrades.go
@@ -0,0 +1,48 @@
+package v4
+
+import (
+	"context"
+	"time"
+
+	sdkmath "cosmossdk.io/math"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/OmniFlix/omniflixhub/v5/app/keepers"
+	"github.com/OmniFlix/omniflixhub/v5/app/upgrades"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+func CreateV5UpgradeHandler(
+	mm *module.Manager,
+	cfg module.Configurator,
+	_ upgrades.BaseAppParamManager,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(context context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		ctx := sdk.UnwrapSDKContext(context)
+		// Run migrations before applying any other state changes.
+		// NOTE: DO NOT PUT ANY STATE CHANGES BEFORE RunMigrations().
+		versionMap, err := mm.RunMigrations(ctx, cfg, fromVM)
+		if err != nil {
+			return nil, err
+		}
+		// Set Gov Params
+		govParams, err := keepers.GovKeeper.Params.Get(ctx)
+		if err != nil {
+			return nil, err
+		}
+		govParams.ExpeditedThreshold = "0.7"
+		expeditedVotingPeriod := time.Hour * 24
+		govParams.ExpeditedVotingPeriod = &expeditedVotingPeriod
+		govParams.ExpeditedMinDeposit = sdk.NewCoins(sdk.NewCoin("uflix", sdkmath.NewIntWithDecimal(2500, 6)))
+
+		err = keepers.GovKeeper.Params.Set(ctx, govParams)
+		if err != nil {
+			return nil, err
+		}
+
+		ctx.Logger().Info("Upgrade complete")
+		return versionMap, nil
+	}
+}
