2254c7c1d65ec23f521915ac55ae1d4c5cfe8e55
Harish Marri <harishmarri551@gmail.com>
2025-03-03 22:13:22 +0530

    chore(deps): bump versions

--- app/upgrades/v012/upgrades.go
+++ app/upgrades/v012/upgrades.go
@@ -12,7 +12,6 @@ import (
 	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	govv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	packetforwardtypes "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v8/packetforward/types"
 	icahosttypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host/types"
 )
 
@@ -49,9 +48,6 @@ func CreateV012UpgradeHandler(
 		}
 
 		keepers.ICAHostKeeper.SetParams(ctx, hostParams)
-
-		// Packet Forward middleware initial params
-		keepers.PacketForwardKeeper.SetParams(ctx, packetforwardtypes.DefaultParams())
 
 		// itc campaigns migrations
 		campaigns := keepers.ItcKeeper.GetAllCampaigns(ctx)
