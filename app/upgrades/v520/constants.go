6bc2acfd99938b6f914a2e69cc6cdc3f04649602
Harish Marri <harishmarri551@gmail.com>
2025-05-12 12:04:43 +0530

    fix app wiring issue

--- app/upgrades/v520/constants.go
+++ app/upgrades/v520/constants.go
@@ -2,7 +2,7 @@ package v520
 
 import (
 	store "cosmossdk.io/store/types"
-	"github.com/OmniFlix/omniflixhub/v5/app/upgrades"
+	"github.com/OmniFlix/omniflixhub/v6/app/upgrades"
 )
 
 const UpgradeName = "v5.2.0"
